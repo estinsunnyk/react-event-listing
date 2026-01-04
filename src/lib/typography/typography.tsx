f73458b9d2ce5da2f876693b543a6cbfdd2568dc
estinsunnyk <estin.sunny@gmail.com>
2026-01-04 17:05:25 +0000

    feat(typography): add typography lib

---
+++ src/lib/typography/typography.tsx
@@ -0,0 +1,23 @@
+import type { JSX } from 'react';
+import styles from './typography.module.css';
+
+type TypographyVariant = 'title' | 'body' | 'caption';
+
+interface TypographyProps {
+  variant?: TypographyVariant;
+  as?: keyof JSX.IntrinsicElements;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Typography({
+  variant = 'body',
+  as,
+  children,
+  className = '',
+}: TypographyProps) {
+  const AsElement = as || 'p';
+  const classList = `${styles[variant]} ${className}`.trim();
+
+  return <AsElement className={classList}>{children}</AsElement>;
+}
